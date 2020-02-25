73fa18ae3571291469067123a88f28233eb9bc6f
shivraj <shivrajsharma21@gmail.com>
2020-02-25 21:47:36 +0530

    mapbox recat

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,32 @@
-import React from "react"
-import mapboxgl from "mapbox-gl"
-import Layout from "../components/layout"
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+
+import React, { Component }  from 'react';
+import ReactMapGL from 'react-map-gl';
 
 
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoic2hpdnJhanNoYXJtYTIxIiwiYSI6ImNrNzIzNjlxbzA1MngzZm53dGxrbXVheGIifQ.d5HraeRq8vDh-0nBiRGuhA';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
-class Map extends React.Component {
-  componentDidMount() {
-    this.map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: "mapbox://styles/mapbox/streets-v11",
-    })
-    AOS.init()
-  }
-
-  componentWillUnmount() {
-    this.map.remove();
-  }
+class Map extends Component {
+ 
+constructor(props){
+  super(props);
+  this.state = {
+    viewport: {
+      width: '100%',
+      height: 400,
+      latitude: 37.7577,
+      longitude: -122.4376,
+      zoom: 8
+    }
+  };
+}
 
   render() {
-    const style = {
-      position: "absolute",
-      top: 0,
-      bottom: 0,
-      width: "100%",
-    }
-
-    return(
-      <Layout>
-     <div data-aos="fade-up"
-    data-aos-offset="200"
-    data-aos-delay="50"
-    data-aos-easing="ease-in-out"
-    data-aos-mirror="true"
-    data-aos-once="false"
-    data-aos-anchor-placement="top-center" style={{height:'400px', position:'relative'}}><div style={style} ref={el => (this.mapContainer = el)}></div></div> 
-     </Layout>
-    )
-    
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapboxApiAccessToken={MAPBOX_TOKEN} />
+    );
   }
 }
-export default Map
+export default Map;
